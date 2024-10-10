bd7a38ced6fdeee4dc90371db88a1f49f9472b85
leo100295 <leonardo.macedo10@escola.pr.gov.br>
2024-10-10 11:40:55 -0300

    dfhj

---
+++ perguntas.js
@@ -0,0 +1,119 @@
+criaCartao(
+    'matemática',
+    'qual o seno de 45?',
+    ‘√2/2’)
+    
+    2
+    criaCartao(
+    'matemática',
+    'fórmula da área de um triângulo ',
+    'multiplicar base por altura e dividir por 2')
+    
+    3
+    criaCartao(
+    'matemática',
+    'qual é a fórmula de bhaskará?',
+    'Δ = b2 – 4ac : 2a’)
+    
+    4
+    criaCartao(
+    'matemática',
+    'fórmula de pitágoras',
+    'a² + b²= c²’)
+    
+    5
+    criaCartao(
+    'matemática',
+    'o que é um número ao quadrado?',
+    'é um número vezes ele mesmo, exemplo 2² = 2 x 2')
+    
+    6
+    criaCartao(
+    'química',
+    'qual é a fórmula para o cálculo do ∆h?',
+    '∆H= Hp - Hr')
+    
+    7
+    criaCartao(
+    'química',
+    'qual o elemento principal da química orgânica?',
+    'carbono')
+    
+    8
+    criaCartao(
+    'quimica',
+    'o que faz uma cadeia ser insaturada?',
+    'ter duplas ou triplas ligações')
+    
+    9
+    criaCartao(
+    'quimica',
+    'que tipo de cadeia não tem grupo funcional?',
+    'hidrocarbonetos')
+    
+    10
+    criaCartao(
+    'quimica',
+    'como se chama uma cadeia que só tem carbono?',
+    'homogenea')
+    
+    11
+    criaCartao(
+    'historia',
+    'quando se iniciou a segunda guerra mundial?',
+    '1 de setembro de 1939')
+    
+    12
+    criaCartao(
+    'história',
+    'quando ocorreu o ataque a Pearl Harbor?',
+    '7 de dezembro de 1941')
+    
+    13
+    criaCartao(
+    'história',
+    'quem foi o primeiro presidente do Brasil?',
+    'Deodoro da Fonseca')
+    
+    14
+    criaCartao(
+    'história',
+    'quando se encerrou a segunda guerra mundial?',
+    '2 de setembro de 1945')
+    
+    15
+    criaCartao(
+    'história',
+    'quem foi considerado o pai dos pobres?',
+    'Getúlio Vargas')
+    
+    16
+    criaCartao(
+    'português',
+    'obra que iniciou o Realismo na literatura',
+    'Madame Bovary')
+    
+    17
+    criaCartao(
+    'portugues',
+    'maior escritor da literatura brasileira',
+    'Machado de Assis')
+    
+    18
+    criaCartao(
+    'português',
+    'com que obra o realismo inicia-se no Brasil?',
+    'Memórias Póstumas de Brás Cubas')
+    
+    19
+    criaCartao(
+    'português',
+    'quantas gerações teve o romantismo?',
+    '3 gerações')
+    
+    
+    20
+    criaCartao(
+    'portugues',
+    'que obra de Machado apresenta o Naturalismo?',
+    'Dom Casmurro')
