565074e601981ba2a105a20b1928504edde00f8d
leo100295 <leonardo.macedo10@escola.pr.gov.br>
2024-10-10 12:08:55 -0300

    dg

--- main.js
+++ main.js
@@ -2,5 +2,15 @@ function criaCartao (categoria, pergunta, resposta){
 let container = document.getElementById('container');
 let cartao = document.createElement('article');
 cartao.className = 'cartao';
-cartao.innerHTML = `
+cartao.innerHTML =  `
+<div class="conteudo-cartao">
+    <h3>categoria</h3>
+<div class="pergunta-cartao">
+    <p>pergunta</p>
+</div>
+<div class="reposta-cartao">
+    <p>respostas</p>
+</div>
+</div>
+
 }
