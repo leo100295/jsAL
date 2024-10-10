7c79bf4e14de4f80eebba4a0973d3e90550c6d58
leo100295 <leonardo.macedo10@escola.pr.gov.br>
2024-10-10 12:21:15 -0300

    n

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let cartao = document.createElement('article');
 cartao.className = 'cartao';
 cartao.innerHTML =  `
 <div class="conteudo-cartao">
-    <h3>categoria</h3>
+    <h3>${categoria}</h3>
 <div class="pergunta-cartao">
     <p>pergunta</p>
 </div>
