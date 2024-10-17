de3431909dc5fd769a14858898004b0f9c5e1ab3
leo100295 <leonardo.macedo10@escola.pr.gov.br>
2024-10-17 11:59:47 -0300

    dg

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ cartao.innerHTML =  `
 <div class="pergunta-cartao">
     <p>${pergunta}</p>
 </div>
-<div class="reposta-cartao">
+<div class="resposta-cartao">
     <p>${resposta}</p>
 </div>
 </div>
