a654fc77698645ab4111eff477e8bd6fae1aeaba
leo100295 <leonardo.macedo10@escola.pr.gov.br>
2024-11-06 08:07:24 -0300

    S

--- pagina2.js
+++ pagina2.js
@@ -5,19 +5,14 @@ function criaCartao (categoria, pergunta, resposta) {
     cartao.className = 'cartao';
     cartao.innerHTML= `
     <div class="conteudo-cartao">
-    <h3>$(categoria}</h3>
+    <h3>${categoria}</h3>
     <div class="pergunta-cartao">
     <p>${pergunta}</p>
     </div>
     <div class="resposta-cartao">
     <p>${resposta}</p>
     </div>
-    </div>
-    `
-let respostaV=false;
-function virar(){
-respostaV=!respostaV;
-cartao.classList.toggle('active', respostaV);
-}
-cartao.addEventListener('click',  virar)
+</div>
+`
     container.appendChild(cartao);
+}
